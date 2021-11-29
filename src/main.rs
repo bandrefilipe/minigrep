108ec72eb34e6260ffb29435abcea5b73b61068e
André Filipe Barranco <b.andrefilipe@gmail.com>
2021-11-29 16:55:08 -0300

    Grouping configuration values

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,24 @@ use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
-    println!("Searching for '{}' in file '{}'", query, filename);
+    let config = Config::new(&args);
+    println!("Searching for '{}' in file '{}'", config.query, config.filename);
 
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file");
     println!("Contents:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config { query, filename }
+    }
 }

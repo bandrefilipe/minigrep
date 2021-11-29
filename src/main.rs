aa1d13736c55ae283d2a75c45f04ce76e1402f40
André Filipe Barranco <b.andrefilipe@gmail.com>
2021-11-29 16:37:06 -0300

    Reading a file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let query = &args[1];
     let filename = &args[2];
     println!("Searching for '{}' in file '{}'", query, filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+    println!("Contents:\n{}", contents);
 }
